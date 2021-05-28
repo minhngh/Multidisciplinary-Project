1c5b258584820814d7172685f69ebb8c5bc51c28
minhngh <minh.nghminh@gmail.com>
2021-05-28 09:45:24 +0700

    add template FCM and edit door state when having a nofication

---
+++ server/fcm.py
@@ -0,0 +1,12 @@
+from pyfcm import FCMNotification
+import yaml
+
+with open('config.yml') as f:
+    config = yaml.load(f, Loader  = yaml.FullLoader)
+
+push_service = FCMNotification(api_key = config['FIREBASE_SERVER_API'])
+ 
+registration_id = config['DEVIDE-IDS']
+message_title = "title"
+message_body = "Unknown"
+result = push_service.notify_multiple_devices(registration_ids=registration_id, message_title=message_title, message_body=message_body)
